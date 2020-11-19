012a9615c0eaf769dea623b98bb8e616b69b2654
Nikolay <slidings@mail.ru>
2020-11-19 09:08:35 +0300

    подключение ServiceLocator

---
+++ Monitor/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,19 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            var service = SimpleIoc.Default;
+        }
+    }
+}
